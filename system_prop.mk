22df52bf659b116c97c43f958c537996c6c917c9
Scavenger-87 <79624609+Scavenger-87@users.noreply.github.com>
2021-06-18 19:53:21 +0200

    Add LMK Settings

--- system_prop.mk
+++ system_prop.mk
@@ -65,3 +65,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.Xinterface=xlan0 \
     ap.interface=wlan0 \
     persist.wlan.ti.calibrated=0
+    
+# LMKd
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true    
